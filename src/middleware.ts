62a37fda2337d87ba9d9f402350504d22282f90b
ryohei884 <pey@ryohei.info>
2025-10-04 02:12:23 +0900

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@
 import { auth } from "@/auth";
 
 export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname === "/category") {
+  if (!req.auth && req.nextUrl.pathname !== "/sign-in") {
     const newUrl = new URL("/sign-in", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
