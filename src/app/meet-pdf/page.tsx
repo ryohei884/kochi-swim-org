6eb67b22f8b97f1a6d7e874d193d2fda5c5182dc
ryohei884 <pey@ryohei.info>
2026-03-10 15:33:24 +0900

    update meet-list

--- src/app/meet-pdf/page.tsx
+++ src/app/meet-pdf/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Page() {
   const pdfURL =
-    "https://nzprheefai1ubld0.public.blob.vercel-storage.com/files/meetlist-G0jXYEAdKbWLE4dMWEcl1jxGXKFO2Y.pdf";
+    "https://nzprheefai1ubld0.public.blob.vercel-storage.com/files/meetlist-IBee3al8YSuDuEzPZGGoq3ZI6UAVyy";
 
   return pdfURL ? (
     <object
