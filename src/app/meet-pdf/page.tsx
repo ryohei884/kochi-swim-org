be3c4095b2b383aaab3b2b433f7bb3d130b61492
ryohei884 <pey@ryohei.info>
2026-03-11 13:56:34 +0900

    updage pdf url

--- src/app/meet-pdf/page.tsx
+++ src/app/meet-pdf/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 export default function Page() {
-  const pdfURL = "/files/meetlist20260309.pdf";
-
-  return pdfURL ? (
+  return (
     <object
       className="w-svw h-svh"
       title="2026年度競技会日程表(2026年3月9日更新)"
-      data={pdfURL}
+      data={`/files/meetlist20260309.pdf`}
       type="application/pdf"
     />
-  ) : (
-    <div>Loading ...</div>
   );
 }
