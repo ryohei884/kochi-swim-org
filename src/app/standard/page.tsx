80e6c39c87dcb1936f2c7ff50c174927ea831e74
ryohei884 <pey@ryohei.info>
2026-04-14 21:35:59 +0900

    add standard page

---
+++ src/app/standard/page.tsx
@@ -0,0 +1,22 @@
+import Standard from "@/components/standard/standard";
+import Footer from "@/components/top/footer";
+import Header from "@/components/top/header";
+
+export default function Live() {
+  return (
+    <>
+      <Header />
+      <div className="bg-white py-24 sm:py-32 dark:bg-gray-900">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:max-w-5xl">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl dark:text-white">
+              資格級・標準記録等
+            </h2>
+            <Standard />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/standard/standard.tsx
@@ -0,0 +1,120 @@
+import { ExternalLink, MinusIcon, PlusIcon } from "lucide-react";
+import Link from "next/link";
+import ReactMarkdown from "react-markdown";
+import remarkBreaks from "remark-breaks";
+import remarkGfm from "remark-gfm";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+
+const faqs = [
+  {
+    title: "2026年度水泳資格表",
+    description: `2026年度の性別・年齢区分別資格級は以下のとおりです。
+- [2026年度水泳資格表](https://nzprheefai1ubld0.public.blob.vercel-storage.com/standard/qualification_standard.pdf)`,
+  },
+  /*
+  {
+    title: "2026年度〇〇大会標準記録",
+    description: `2026年度の年齢・性別資格級は以下のとおりです。
+- [2026年度水泳資格表](https://nzprheefai1ubld0.public.blob.vercel-storage.com/registration/Initial_Registration_Manual_2026.pdf)`,
+  },
+    {
+    title: "2026年度泳力検定合格基準",
+    description: `2026年度の年齢・性別資格級は以下のとおりです。
+- [2026年度水泳資格表](https://nzprheefai1ubld0.public.blob.vercel-storage.com/registration/Initial_Registration_Manual_2026.pdf)`,
+  },
+  */
+];
+
+export default function Standard() {
+  const AnchorTag = ({ node, children, ...props }: any) => {
+    try {
+      props.href = URL.parse(props.href) ? props.href : "";
+      props.target = "_blank";
+      props.rel = "noopener noreferrer";
+    } catch (e) {}
+    return (
+      <Link {...props} className="flex items-center underline">
+        {children}
+        <ExternalLink className="h-4 ml-1" />
+      </Link>
+    );
+  };
+
+  const UListTag = ({ node, children, ...props }: any) => {
+    return (
+      <ul {...props} className="list-disc list-outside pl-[1em] mb-[1em]">
+        {children}
+      </ul>
+    );
+  };
+
+  const OListTag = ({ node, children, ...props }: any) => {
+    return (
+      <ol {...props} className="list-decimal list-outside pl-[1em] mb-[1em]">
+        {children}
+      </ol>
+    );
+  };
+
+  const PTag = ({ node, children, ...props }: any) => {
+    return (
+      <p {...props} className="mb-[1em]">
+        {children}
+      </p>
+    );
+  };
+
+  return (
+    <dl className="mt-16 divide-y divide-gray-900/10 dark:divide-white/10">
+      {faqs.map((faq) => (
+        <Collapsible
+          defaultOpen={true}
+          key={faq.title}
+          className="py-6 first:pt-0 last:pb-0"
+        >
+          <div>
+            <dt>
+              <CollapsibleTrigger className="group flex w-full items-start justify-between text-left text-gray-900 dark:text-white">
+                <span className="text-base/7 font-semibold">{faq.title}</span>
+                <span className="ml-6 flex h-7 items-center">
+                  <PlusIcon
+                    aria-hidden="true"
+                    className="size-6 group-data-open:hidden"
+                  />
+                  <MinusIcon
+                    aria-hidden="true"
+                    className="size-6 group-not-data-open:hidden"
+                  />
+                </span>
+              </CollapsibleTrigger>
+            </dt>
+          </div>
+          <CollapsibleContent className="mt-2 pr-12">
+            <dd className="prose">
+              <ReactMarkdown
+                remarkPlugins={[remarkGfm, remarkBreaks]}
+                components={{
+                  a: AnchorTag,
+                  ul: UListTag,
+                  ol: OListTag,
+                  p: PTag,
+                }}
+              >
+                {faq.description}
+              </ReactMarkdown>
+            </dd>
+          </CollapsibleContent>
+        </Collapsible>
+      ))}
+    </dl>
+  );
+}
+
+// <p className="text-base/7 text-gray-600 dark:text-gray-400">
+// {faq.description}
+// </p>
