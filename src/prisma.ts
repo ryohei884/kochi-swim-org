4a37bd23fa589a2f883ad3663f6b8fbb0a5645af
ryohei884 <pey@ryohei.info>
2026-05-25 08:06:29 +0900

    update prisma.tx

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,9 +1,17 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 
-import { PrismaClient } from "@/app/generated/prisma/client";
+// import { PrismaClient } from "@/app/generated/prisma/client";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DIRECT_DATABASE_URL!,
 });
 
-export const prisma = new PrismaClient({ adapter });
+// export const prisma = new PrismaClient({ adapter });
+
+import { PrismaClient } from "@/app/generated/prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
